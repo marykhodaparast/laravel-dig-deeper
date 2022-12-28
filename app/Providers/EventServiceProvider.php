ce4f5a13c4e1a49fce844deb491dd01613afb045
maryam <50619466+marykhodaparast@users.noreply.github.com>
2022-12-28 15:34:15 +0330

    learning queue and event but not completing learning

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use App\Events\PodcastProcessed;
 use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
@@ -27,6 +28,13 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Event::listen(
+            PodcastProcessed::class,
+            [SendPodcastNotification::class, 'handle']
+        );
+
+        Event::listen(function (PodcastProcessed $event) {
+            //
+        });
     }
 }
